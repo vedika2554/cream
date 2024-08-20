e8018bf731968508f6bceab705590d3864e13291
vedika2554 <vedikayadav652@gmail.com>
2024-08-20 14:51:00 +0530

    2nd

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Navbar.css"
+import { Link } from "react-router-dom";
 
 function Navbar(){
     return (
         <div className="nav"><span className="bn">🍦icecream</span>
         <div className="nl">
-            <a href="/"className="nl">Home</a>
-            <a href="/about"className="nl">About</a>
-            <a href="/contact"className="nl">Contact</a>
+            <Link to="/"className="nl">Home</Link>
+            <Link to="/about"className="nl">About</Link>
+            <Link to="/contact"className="nl">Contact</Link>
         </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const cupt = window.location.pathname;
-
-if (cupt=='/'){
-  root.render(<Home/>)
-}
-else if(cupt=="/about"){
-  root.render(<About/>)
-}
-else if(cupt=="/contact"){
-  root.render(<Contact/>)
-}
 
 
+const router = createBrowserRouter([
+  {
+    "path":'/',
+    "element":<Home/>
+  },
+  {
+    "path":'/about',
+    "element":<About/>
+  },
+  {
+    "path":'/contact',
+    "element":<Contact/>
+  }
+
+])
+
+root.render(<RouterProvider router={router}/>)
 
